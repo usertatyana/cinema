2de56ce156bc82176be907220ddd7b6be3923432
Tatyana Kapina <deniscapin@yandex.ru>
2023-04-12 18:28:03 +0300

    add burger menu

---
+++ methed_cinema_js/js/index.js
@@ -0,0 +1,9 @@
+import slideMenu from './menu.js';
+
+
+slideMenu({
+    openBtn: '.header__burger-btn',
+    menu: '.navigation',
+    classActiveMenu: 'navigation_active',
+    closeTrigger: '.navigation__link, .navigation__close',
+});
